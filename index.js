75f4403c345e077e333553c042cf11152263c29d
Anna Schmidt <marshall1697@gmail.com>
2022-09-06 17:30:34 -0600

    commit tree f8458fbbf72f78d11a74509fa2aed2063ff7c973

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+// const superbowlWin = (array) => {
+//    const winObj = array.find(item => item.result === 'W');
+//    return winObj.year;
+
+//   }
+
+const record = [
+    { year: "2018", result: "N/A"},
+    { year: "2017", result: "N/A"},
+    { year: "2016", result: "N/A"},
+    { year: "2015", result: "l"},
+    { year: "2014", result: "N/A"},
+  ]
+
+const superbowlWin = (array) => {
+  let winningYear = array.find((obj) => obj.result === 'W');
+  if(winningYear !== undefined){
+    return winningYear.year
+    
+  }
+    
+};
+    
+superbowlWin(record);
+    
+
